c571d6a8d7f19003f16649b2b8a649dcd610ec0b
Krishna Ramdeep <krishna.ramdeep@gmail.com>
2017-01-01 17:40:00 -0600

    Finished lesson 1

--- SUNSHINE/S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ SUNSHINE/S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -21,6 +21,7 @@ import android.support.v7.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     // TODO (1) Create a field to store the weather display TextView
+    private TextView wDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +29,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_forecast);
 
         // TODO (2) Use findViewById to get a reference to the weather display TextView
+        wDisplay = (TextView) findViewById(R.id.tv_weather_data);
 
         // TODO (3) Create an array of Strings that contain fake weather data
+        String[] dummyWeatherData = {
+                "Today, May 17 - Clear - 17°C / 15°C",
+                "Tomorrow - Cloudy - 19°C / 15°C",
+                "Thursday - Rainy- 30°C / 11°C",
+                "Friday - Thunderstorms - 21°C / 9°C",
+                "Saturday - Cloudy - 16°C / 7°C",
+                "Sunday - Rainy - 16°C / 8°C",
+                "Monday - Partly Cloudy - 15°C / 10°C",
+                "Tue, May 24 - Rainy - 16°C / 18°C",
+                "Wed, May 25 - Cloudy - 19°C / 15°C",
+                "Thu, May 26 - Stormy - 30°C / 11°C",
+                "Fri, May 27 - Hurricane - 21°C / 9°C",
+                "Sat, May 28 - Meteors - 16°C / 7°C",
+                "Sun, May 29 - Storm - 16°C / 8°C",
+                "Mon, May 30 - Post Storm - 15°C / 10°C",
+        };
 
         // TODO (4) Append each String from the fake weather data array to the TextView
+        for (String w : dummyWeatherData) {
+        	wDisplay.append(w + "\n\n\n");
+        }
     }
 }
